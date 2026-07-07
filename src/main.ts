1f6e4b36260a49f5f38c72d1ba15c3a13bd35454
eneru-m <eloimontagutmarti@gmail.com>
2026-07-07 22:37:55 +0200

    searchCards lang optional & start open

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { searchCards } from "./services/scryfall-service";
 
 async function main(): Promise<void> {
     try {
-        const result = await searchCards(
-            "Lightning Bolt"
-        );
+        const result = await searchCards("Lightning Bolt", 'es');
         console.log(result);
     }
     catch (error) {
--- src/services/scryfall-service.ts
+++ src/services/scryfall-service.ts
@@ -1,14 +1,9 @@
 /**
  * searchCards
  */
-export async function searchCards(cardName: string): Promise<any> {
-    const language = "en";
-
+export async function searchCards(cardName: string, language: string = 'en'): Promise<any> {
     const url =
-        `https://api.scryfall.com/cards/search` +
-        `?order=released` +
-        `&include_extras=true` +
-        `&q=name=${encodeURIComponent(cardName)} lang:${language}`;
+        `https://api.scryfall.com/cards/search?order=released&include_extras=true&q=name=${encodeURIComponent(cardName)} lang:${language}`;
 
     const response = await fetch(url);
 
